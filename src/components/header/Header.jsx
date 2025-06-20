bad00bc813a272856141305a1664c93f7141bc2c
ankit-548 <raghuwanshi.ankit548@gmail.com>
2025-06-20 09:45:32 +0530

    feat: update Header component to include search input and dynamic navigation based on user login status; enhance index.js to export Logo and Logout components

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,33 @@
-import Logo from '../Logo.jsx'
+import { Link } from 'react-router-dom';
+import { Logo, Input } from '../index.js'
+import { useSelector } from 'react-redux';
 
 function Header() {
-    const navBar = [
+    const isUserLoggedIn = useSelector(state => state.isLoggedIn)
 
+    const navBar = [
+        {
+            name: 'SignIn',
+            slug: '/login',
+            isActive: isUserLoggedIn,
+        }
     ]
 
     return (
         <>
-            <Logo></Logo>
+            <ul>
+                <li>
+                    <Logo></Logo>
+                </li>
+                <li>                    
+                    <Input placeholder="Search" className="rounded-4xl w-25"/>
+                </li>
+                {
+                    navBar.map(item => {
+                        return item.isActive && <li><Link to={`/${item.slug}`}>{item.name}</Link></li>
+                    })
+                }                
+            </ul>
         </>
     )
 }
--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,15 @@ import Input from "./Input.jsx";
 import Register from "./Register.jsx";
 import Login from "./Login.jsx"
 import Container from "./container/Container.jsx";
+import Logout from "./Logout.jsx"
+import Logo from "./Logo.jsx"
 
 export {
     Button,
     Input,
     Register,
     Login,
-    Container
+    Container,
+    Logout,
+    Logo
 }
