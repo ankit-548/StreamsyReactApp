f9e13200f67e103c041255239c76a3eb07cb6fe1
ankit-548 <raghuwanshi.ankit548@gmail.com>
2025-07-07 09:47:02 +0530

    fix: update Input component to improve label rendering and maintain consistent formatting

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,18 +1,19 @@
 import { useId } from "react";
 import React from "react";
 
-function Input(
+function Input({
     label,
     type,
     placeholder,
     className,
     ref,
     ...props
-) {
+}) {
     const id = useId();
+    console.log('label',label)
     return (
         <>
-            {label && (<label className="m-1 px-4 py-1">{label}</label>)}
+            {label && (<label className="m-1 px-4 py-1">{`${label}`}</label>)}
             <input type={`${type}`} placeholder={`${placeholder}`} className={`m-1 py-1 px-4 rounded-xl ${className}`} ref={ref} id={id} {...props}/>
         </>
     )
